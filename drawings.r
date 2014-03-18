f58d2245843bc2590edf21ff66c7a41fbdbfd6dc
Thomas Levine <_@thomaslevine.com>
2014-03-18 15:24:05 +0000

    titles

--- drawings.r
+++ drawings.r
@@ -4,9 +4,19 @@ baseplot <- function(main, formula = (Petal.Length ~ Petal.Width), data = iris) 
     col.axis = 'grey', col.lab = 'grey', col.main = 'grey', fg = 'grey')
 }
 
-#pdf('covariance.pdf')
-baseplot('Two iris variables')
+interjection <- function(main) {
+  plot.new()
+  text(0.5, 0.5, cex = 2, label = main, font = 2)
+}
 
-baseplot('How')
+#pdf('covariance.pdf')
+interjection('What is a statistic?')
+baseplot('Two iris variables that move together')
+rand <- data.frame(x = rnorm(100), y = rnorm(100))
+baseplot('Normal random noise', formula = y ~ x, data = rand)
+baseplot('')
+interjection('We want a number\nthat describes\nwhether two variables\nmove together.')
+
+#baseplot('How')
 #points(Petal.Length ~ Petal.Width, data = iris)
 #dev.off()
