95e0efc1b643679842cf7bc8ff63228571c1e97d
Thomas Levine <_@thomaslevine.com>
2014-03-18 15:16:11 +0000

    colors

--- drawings.r
+++ drawings.r
@@ -1,7 +1,12 @@
-baseplot <- function() {
-  plot(Petal.Length ~ Petal.Width, data = iris, type = 'n', bty = 'n')
+baseplot <- function(main) {
+  plot(Petal.Length ~ Petal.Width, data = iris, type = 'p', bty = 'n',
+    main = main, pch = 21, bg = 'grey', col = NULL,
+    col.axis = 'grey', col.lab = 'grey', col.main = 'grey', fg = 'grey')
 }
 
 #pdf('covariance.pdf')
-baseplot()
+baseplot('Two iris variables')
+
+#baseplot('')
+#points(Petal.Length ~ Petal.Width, data = iris)
 #dev.off()
